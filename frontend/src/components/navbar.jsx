3577683718c8605ba17e8b8fde5200a66fd1f40c
FabriVal <fabriziogabrielvallone@gmail.com>
2024-01-31 14:00:20 -0300

    Actualize la interfaz principal. 31 01 2024

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 
 const Navbar = () => {
     return (
-
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-            <h1 className='w-full text-3xl font-bold text-[#E5CFF7]'>REACT</h1>
+        <div className='bg-[#5B0888]'>
+          <div className='flex justify-between items-center h- max-w-[1240px] mx-auto px-4 text-white'>
+            <h1 className='w-full text-3xl font-bold text-[#E5CFF7]'>Analytix</h1>
             <ul className='flex'>
-            <li className='p-4' style={{ whiteSpace: 'nowrap' }}>Iniciar Sesion</li>
-                <li className='p-4'>Registrarse</li>
+              <li className='p-4' style={{ whiteSpace: 'nowrap' }}>Iniciar Sesion</li>
+              <li className='p-4'>Registrarse</li>
             </ul>
+          </div>
         </div>
-
-    )
-}
+      );
+    };
 
 export default Navbar;
