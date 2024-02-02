ea82eb74b5abac4b87b78bc0a6fb0c94f53dc772
FabriVal <fabriziogabrielvallone@gmail.com>
2024-02-02 10:57:37 -0300

    Termine la estructura basica del primer componente de descripcion 02 02 2024

--- frontend/src/components/introduction.jsx
+++ frontend/src/components/introduction.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import excel from '../assets/excel.png';
+import '../styles/styles.css';
 
 const Introduction = () => {
-    return (
-      <div className="w-full mt-7 bg-white py-16 px-4">
-        Introduction
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Trigger the effect only once
+  });
+
+  return (
+    <div ref={ref} className="flex items-center justify-center mt-20 bg-white">
+      <div className={`max-w-screen-lg mx-auto flex fade-in ${inView ? 'visible' : ''}`}>
+        {/* Image on the left */}
+        <div className="flex-shrink-0">
+          <img
+            src={excel}
+            alt="Introduction Image"
+            className="w-[400px] h-[400px] my-4 object-cover max-w-1/2"
+          />
+        </div>
+
+        {/* Text block on the right */}
+        <div className="my-10 ml-7 flex-grow">
+          <h2 className="text-center text-2xl font-bold mb-2">Simplifica la forma de procesar tus datos</h2>
+          <p className="text-center text-gray-600 mt-20">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+
 
 export default Introduction;
